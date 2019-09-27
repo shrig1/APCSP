2d405237c8649080f7a0f1f82555c95200b5d411
Shricoding25 <42702641+Shricoding25@users.noreply.github.com>
2019-09-26 21:50:28 -0500

    brought back layout

--- APCSPyeet/APCSPyeet/APCSPyeet/views.py
+++ APCSPyeet/APCSPyeet/APCSPyeet/views.py
@@ -16,22 +16,3 @@ def home():
         year=datetime.now().year,
     )
 
-@app.route('/contact')
-def contact():
-    """Renders the contact page."""
-    return render_template(
-        'contact.html',
-        title='Contact',
-        year=datetime.now().year,
-        message='Your contact page.'
-    )
-
-@app.route('/about')
-def about():
-    """Renders the about page."""
-    return render_template(
-        'about.html',
-        title='About',
-        year=datetime.now().year,
-        message='Your application description page.'
-    )
